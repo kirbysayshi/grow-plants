e078ba50e933d4cf239edca542677c7161ce78a5
Andrew Petersen <kirbysayshi@gmail.com>
2015-10-25 11:43:12 -0400

    add a combine reducers thing

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ let store = createStore((state, action) => {
       },
       plants: [
         ...state.plants,
-        { height: 0, age: 1 }
+        { height: 0, age: 1, emotions: {} }
       ]
     }
   }
@@ -225,4 +225,24 @@ function createStore (reducer, initialState) {
   }
 
   return store;
+}
+
+function combineReducers (reducers) {
+  const keys = Object.keys(reducers);
+  return function (state, action) {
+    let hasChanged = false;
+    const nextFullState = {};
+    keys.forEach(k => {
+      const prevStateForKey = state[k];
+      const stateForKey = reducers[k](state, action);
+      if (stateForKey === undefined) {
+        throw new Error('Reducer for `' + k + '` returned `undefined`');
+      }
+      if (!prevStateForKey !== stateForKey) {
+        hasChanged = true;
+        nextFullState[k] = stateForKey;
+      }
+    });
+    return hasChanged ? nextFullState : state;
+  }
 }
